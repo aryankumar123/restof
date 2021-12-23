b61833c2db5845354a56dcee76ddb377cf941bde
aryankumar123 <aryankumarben@gmail.com>
2021-12-23 19:56:49 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+import { FaceComponent } from './face/face.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    
+
+    FaceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order-items/order-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-items',
+  templateUrl: './order-items.component.html',
+  // styleUrls: ['./order-items.component.css']
+})
+export class OrderItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
